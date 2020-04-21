e3378c75ffbfc62a4428eff701f3aa3fe7f9598b
TranMinhKhuong <tranminhkhuongph08470@gmail.com>
2020-04-21 10:42:42 +0700

    2 files modified

--- src/app/schools-form/schools-form.component.ts
+++ src/app/schools-form/schools-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup } from "@angular/forms";
 import { Router, ActivatedRoute } from "@angular/router";
 import { SchoolService } from "../school.service";
+import {Validators } from '@angular/forms';
 @Component({
   selector: "app-schools-form",
   templateUrl: "./schools-form.component.html",
@@ -10,12 +11,17 @@ import { SchoolService } from "../school.service";
 export class SchoolsFormComponent implements OnInit {
   schoolForm = new FormGroup({
     id: new FormControl(null),
-    name: new FormControl(""),
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(25),
+      Validators.maxLength(200)
+    ]),
     logo: new FormControl(""),
     address: new FormControl(""),
     president: new FormControl(""),
     province: new FormControl("")
   });
+  get name() { return this.schoolForm.get('name');}
 
   constructor(
     private SchoolService: SchoolService,
@@ -33,6 +39,9 @@ export class SchoolsFormComponent implements OnInit {
     });
   }
   save() {
+    if(this.schoolForm.invalid){
+      return false;
+    }
     if (this.schoolForm.value.id == null) {
       //thêm mới
       this.SchoolService.addNewSchool(this.schoolForm.value).subscribe(data => {
